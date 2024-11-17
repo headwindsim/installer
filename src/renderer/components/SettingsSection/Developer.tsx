a3ba92964b864b080d56007ec5f7127a5f8e3e19
Denis Freund <info@denis-freund.de>
2024-11-17 14:14:21 +0100

    refactor: enhance configuration url handle

--- src/renderer/components/SettingsSection/Developer.tsx
+++ src/renderer/components/SettingsSection/Developer.tsx
@@ -1,12 +1,16 @@
 import React, { FC, useCallback, useEffect, useState } from 'react';
 import { useSetting } from 'renderer/rendererSettings';
 import { Toggle } from 'renderer/components/Toggle';
+import * as packageInfo from '../../../../package.json';
+import { Button, ButtonType } from 'renderer/components/Button';
+import { ipcRenderer } from 'electron';
+import channels from 'common/channels';
 
 const SettingsItem: FC<{ name: string }> = ({ name, children }) => (
-  <div className="flex flex-row items-center justify-between py-3.5">
-    <p className="m-0">{name}</p>
-    {children}
-  </div>
+    <div className="flex flex-row items-center justify-between py-3.5">
+      <p className="m-0">{name}</p>
+      {children}
+    </div>
 );
 
 export const DeveloperSettings: React.FC = () => {
@@ -17,7 +21,7 @@ export const DeveloperSettings: React.FC = () => {
 
   const validateUrl = useCallback(() => {
     try {
-      fetch(configDownloadUrl, { cache: "no-store" }).then((response) => {
+      fetch(configDownloadUrl).then((response) => {
         setConfigDownloadUrlValid(response.status === 200);
       });
     } catch (e) {
@@ -30,29 +34,43 @@ export const DeveloperSettings: React.FC = () => {
   }, [validateUrl]);
 
   return (
-    <div>
-      <div className="flex flex-col">
-        <h2 className="text-white">General Settings</h2>
-        <div className="flex flex-col divide-y divide-gray-600">
-          <SettingsItem name="Configuration Download URL">
-            <div className="flex flex-row items-center justify-between py-2 text-white">
-              <input
-                className={` text-right text-xl ${configDownloadUrlValid ? 'text-green-500' : 'text-red-500'}`}
-                value={configDownloadUrl}
-                type="url"
-                onChange={(event) => setConfigDownloadUrl(event.target.value)}
-                onBlur={() => validateUrl()}
-                size={50}
-              />
-            </div>
-          </SettingsItem>
-          <SettingsItem name="Force Use Local Configuration">
-            <div className="flex flex-row items-center justify-between text-white">
-              <Toggle value={configForceUseLocal} onToggle={setConfigForceUseLocal} />
-            </div>
-          </SettingsItem>
+      <div>
+        <div className="flex flex-col">
+          <h2 className="text-white">General Settings</h2>
+          <div className="flex flex-col divide-y divide-gray-600">
+            <SettingsItem name="Configuration Download URL">
+              <div className="flex flex-row items-center justify-between py-2 text-white">
+                <input
+                    className={` text-right text-xl ${configDownloadUrlValid ? 'text-green-500' : 'text-red-500'}`}
+                    value={configDownloadUrl}
+                    type="url"
+                    onChange={(event) => setConfigDownloadUrl(event.target.value)}
+                    onBlur={() => validateUrl()}
+                    size={50}
+                />
+                <Button
+                    type={ButtonType.Neutral}
+                    className="ml-2 h-fit min-h-10 text-lg"
+                    onClick={() => ipcRenderer.send(channels.window.reload)}
+                >
+                  Reload Installer
+                </Button>
+                <Button
+                    type={ButtonType.Neutral}
+                    className="ml-2 h-fit min-h-10 text-lg"
+                    onClick={() => setConfigDownloadUrl(packageInfo.configUrls.production)}
+                >
+                  Reset to default
+                </Button>
+              </div>
+            </SettingsItem>
+            <SettingsItem name="Force Use Local Configuration">
+              <div className="flex flex-row items-center justify-between text-white">
+                <Toggle value={configForceUseLocal} onToggle={setConfigForceUseLocal} />
+              </div>
+            </SettingsItem>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
